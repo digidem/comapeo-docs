fc1305dde97202ad8e725f715614c613848cf60a
Tomás Ciccola <tciccola@digital-democracy.com>
2025-06-04 11:39:12 -0300

    add path to docs in docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -127,6 +127,7 @@ const config: Config = {
       'classic',
       {
         docs: {
+          path: 'docs',
           sidebarPath: './src/components/sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
